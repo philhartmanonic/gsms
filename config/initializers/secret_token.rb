9d1a465b573a7ccba75ef08ad8c762bdf7ff55d2
Galen Burghardt <galen.w.burghardt@gmail.com>
2015-09-28 15:47:10 -0500

    baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gsms::Application.config.secret_key_base = '006f458dc6ff8eac9caf2deed9ebfe877ccb4969b04fe377b65146c2a443bd7b2a5e8a069b65ffd7c947dce9e1e9de5e4cc3feb1f86300bf0b57369d8f30ccfb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gsms::Application.config.session_store :cookie_store, key: '_gsms_session'
